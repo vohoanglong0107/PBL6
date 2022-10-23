c4f5c5def387ad1d364e30512633a6aeab8a5abc
Võ Hoàng Long <78085736+vohoanglong0107@users.noreply.github.com>
2022-10-23 13:40:39 +0000

    feat(infra): disable cloud logging for gke
    For small cluster sizes, the cloud logging agent is not needed, but it
    also eat up a lot of resources.

--- deploy/terraform/README.md
+++ deploy/terraform/README.md
@@ -7,3 +7,11 @@ With remote state on GCS
 ```bash
 terraform -chdir=deploy/terraform init -backend-config="bucket=<BUCKET_NAME>"
 ```
+
+## Apply terraform
+
+```bash
+terraform -chdir=deploy/terraform apply
+```
+
+---
--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -133,6 +133,9 @@ module "gke" {
   http_load_balancing    = false
   enable_private_nodes   = true
 
+  // disable logging for small cluster
+  logging_service = "none"
+
   grant_registry_access = true
 
   remove_default_node_pool = true
