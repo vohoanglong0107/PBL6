ad8e61b123a648ba9abfb2baa54202412849d62e
Huy Nguyen <nguyenhuy19092001@gmail.com>
2022-11-05 16:23:20 +0700

    feat(web): change-banner

--- apps/web/src/components/Banner/Banner.tsx
+++ apps/web/src/components/Banner/Banner.tsx
@@ -7,14 +7,12 @@ import Head from "next/head";
 
 export default function Banner() {
   return (
-    <>
-      <Image
-        className="max-w-xs h-auto"
-        src={Logo2}
-        alt="image description"
-        height={700}
-        width={700}
-      />
-    </>
+    <Image
+      className="max-w-xs h-auto"
+      src={Logo2}
+      alt="image description"
+      height={700}
+      width={700}
+    />
   );
 }
